f8b85dcb9b28b5d070c635faf992f11bf3c711be
codekappal <138691869+codekappal@users.noreply.github.com>
2025-03-07 02:27:55 +0530

    updated cards with finance

--- config/data.ts
+++ config/data.ts
@@ -5,21 +5,21 @@ export const cardData = [
       title: "The Future of Artificial Intelligence",
       description:
         "Artificial intelligence is transforming industries, from healthcare to finance. Learn how AI is shaping the future.",
-      imageUrl: "/images/rocket/2.jpg",
+      imageUrl: "/images/cards/1.jpg",
       readMoreLink: "#",
     },
     {
-      title: "Sustainable Energy Solutions",
+      title: "Make Better Decisions with Analytics",
       description:
-        "Discover the latest advancements in renewable energy and how they are helping combat climate change.",
-      imageUrl: "/images/rocket/4.jpg",
+        "Unlock the power of data-driven insights to make smarter, faster, and more impactful decisions for your business.",
+      imageUrl: "/images/cards/2.jpg",
       readMoreLink: "#",
     },
     {
-      title: "Exploring Space: The Next Frontier",
+      title: "Master Your Personal Finance",
       description:
-        "Space exploration is reaching new heights with missions to Mars and beyond. Find out what's next for humanity in space.",
-      imageUrl: "/images/rocket/5.jpg",
+        "Take control of your financial future with smart strategies and tools. Learn how to budget, save, and invest for long-term success.",
+      imageUrl: "/images/cards/3.jpg",
       readMoreLink: "#",
     },
   ];
