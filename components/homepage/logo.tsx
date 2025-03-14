5b5c576fedc0d66c9c8fa7fc71d9bb382491e90e
codekappal <138691869+codekappal@users.noreply.github.com>
2025-03-14 22:47:28 +0530

    updated logo

--- components/homepage/logo.tsx
+++ components/homepage/logo.tsx
@@ -4,9 +4,8 @@ import Image from "next/image";
 export const Logo: React.FC = () => {
   return (
     <NextLink className="flex justify-start items-center gap-1" href="/">
-      <Image alt="Ayspire Inc" height={40} src="/logo.svg" width={40} />
-      {/* <Logo className="text-primary" /> */}
-      <p className="font-bold text-inherit text-xl text-primary">Ayspire Inc</p>
+      <Image alt="Ayspire Inc" height={125} src="/logo.png" width={125} />
+      {/* <p className="font-bold text-inherit text-xl text-primary">Ayspire Inc</p> */}
     </NextLink>
   );
 };
